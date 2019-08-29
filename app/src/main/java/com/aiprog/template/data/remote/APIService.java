2d047809f049cea5c8399d2ef4c5364d4f8c1e1e
Arvindo <arvindomondal@gmail.com>
2019-08-30 00:06:29 +0530

    api

--- app/src/main/java/com/aiprog/template/data/remote/APIService.java
+++ app/src/main/java/com/aiprog/template/data/remote/APIService.java
@@ -6,10 +6,14 @@ import com.aiprog.template.data.model.apis.flag.FlagApi;
 import io.reactivex.Flowable;
 import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.Part;
+import retrofit2.http.Query;
 
 /**
  * Author       : Arvindo Mondal
@@ -27,5 +31,21 @@ public interface APIService {
             @Part MultipartBody.Part file
     );
 */
+/*
+    @GET("/api/MobileApi/TaksCompleted?")
+    Call<String> setUserProfileData(
+            @Query("mobile") String mobile,
+            @Query("TaskId") String taskId
+    );
+
+    @FormUrlEncoded
+    @POST("/api/MobileApi/GetDetails")
+    Call<String> details(
+            @Field("Mobile") String mobile,
+            @Field("type") String walletType,
+            @Field("PageIndex") String pageIndex,
+            @Field("TopCount") String topCount
+    );
+    */
 
 }
